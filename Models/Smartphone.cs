efc0b8f7cd5fad5789cf17a12c4c3d9841e221e9
Miriam Bastos <miriamrbastos1@gmail.com>
2024-05-05 19:49:36 -0300

    :tada: Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get;  set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama 
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
-
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"NK1220", imei:"111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Github");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "987654", modelo: "PlusX", imei: "222222", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
